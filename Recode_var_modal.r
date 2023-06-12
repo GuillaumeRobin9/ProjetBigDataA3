6bd5d02c6b257d3eb7126099a8bd00afdaf4599d
epeigne <peigne.enzo@gmail.com>
2023-06-12 14:05:59 +0200

    modif place=NULL par place=0 pour piétons

--- Recode_var_modal.r
+++ Recode_var_modal.r
@@ -3,7 +3,7 @@ data <- read.csv("data/stat_acc_V3.csv", sep=";")
 
 #print les diffrentes valeurs de chaque colonne multimodale
 # print(unique(data$descr_cat_veh))
-print(unique(data$descr_grav))
+# print(unique(data$descr_grav))
 
 #creation d'un vecteur de valeurs a remplacer a partir des valeurs de unique(data$colonne)
 old_cat_veh_values <- unique(data$descr_cat_veh)
@@ -18,6 +18,20 @@ for (i in 1:length(old_cat_veh_values)) {
   data$descr_cat_veh <- replace(data$descr_cat_veh, data$descr_cat_veh == old_cat_veh_values[i], new_cat_veh_values[i])
   data$descr_grav <- replace(data$descr_grav, data$descr_grav == old_descr_grav_values[i], new_descr_grav_values[i])
 }
+
+
+#---------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------MODIF CASES VIDES-----------------------------------------------
+#---------------------------------------------------------------------------------------------------------------
+
+#boucle pour remplacer les cases NULL par 0 dans la colonne "place"
+for (i in 1:length(data$place)) {
+  if (data$place[i] == "NULL") {
+    data$place[i] <- 0
+  }
+}
+
+
 
 #export csv
 write.table(data, file = "data/stat_acc_V3_modifModal.csv", sep = ";", row.names = FALSE, col.names = TRUE, quote = FALSE)
