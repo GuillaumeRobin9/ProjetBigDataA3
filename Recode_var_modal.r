11f59e8bd3e76f7baa6fcb0e7608d88527038fe1
epeigne <peigne.enzo@gmail.com>
2023-06-13 08:36:46 +0200

    Update modif cat veh with good numbers

--- Recode_var_modal.r
+++ Recode_var_modal.r
@@ -9,16 +9,17 @@ data <- read.csv("data/stat_acc_V3.csv", sep=";")
 old_cat_veh_values <- unique(data$descr_cat_veh)
 old_descr_grav_values <- unique(data$descr_grav)
 
-#creation d'un vecteur de valeurs de remplacement
-new_cat_veh_values <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)
-new_descr_grav_values <- c(1,2,3,4)
+#creation dictionnaire clé valeur avec les valeurs trouvées dans old_cat_veh_values
+cat_veh_list <- list("PL seul > 7,5T" = 14, "VU seul 1,5T <= PTAC <= 3,5T avec ou sans remorque " = 10, "VL seul" = 07, "Autocar" = 38, "PL > 3,5T + remorque" = 15, "Cyclomoteur <50cm3" = 02, "Motocyclette > 125 cm3" = 33, "Tracteur routier + semi-remorque" = 17, "Tracteur agricole" = 21, "PL seul 3,5T <PTCA <= 7,5T" = 13, "Autobus" = 37, "Scooter > 50 cm3 et <= 125 cm3" = 32, "Train" = 39, "Scooter > 125 cm3" = 34, "Scooter < 50 cm3" = 30, "Voiturette (Quadricycle à moteur carrossé) (anciennement \"voiturette ou tricycle à moteur\")" = 03, "Autre véhicule" = 99, "Bicyclette" = 01, "Motocyclette > 50 cm3 et <= 125 cm3" = 31, "Engin spécial" = 20, "Quad lourd > 50 cm3 (Quadricycle à moteur non carrossé)" = 36, "Tramway" = 19, "Tracteur routier seul" = 16, "Quad léger <= 50 cm3 (Quadricycle à moteur non carrossé)" = 35)
 
-#boucle for pour remplacer les valeurs de old_values par les valeurs de new_values
-for (i in 1:length(old_cat_veh_values)) {
-  data$descr_cat_veh <- replace(data$descr_cat_veh, data$descr_cat_veh == old_cat_veh_values[i], new_cat_veh_values[i])
-  data$descr_grav <- replace(data$descr_grav, data$descr_grav == old_descr_grav_values[i], new_descr_grav_values[i])
+#boucle pour remplacer les valeurs de la colonne "descr_cat_veh" par les valeurs du dictionnaire correspondantes
+for (i in 1:length(data$descr_cat_veh)) {
+  for (j in 1:length(cat_veh_list)) {
+    if (data$descr_cat_veh[i] == names(cat_veh_list[j])) {
+      data$descr_cat_veh[i] <- cat_veh_list[[j]]
+    }
+  }
 }
-
 
 #---------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------MODIF CASES VIDES-----------------------------------------------
@@ -74,18 +75,18 @@ arr <- unique(data$ville[data$latitude == 2009])
 
 #stock les arrondissements de paris, de marsseille et de lyon dans 3 vecteurs
 arr_paris <- c("PARIS 20", "PARIS 19", "PARIS 18", "PARIS 17", "PARIS 16", "PARIS 15", "PARIS 14", "PARIS 13", "PARIS 12", "PARIS 11", "PARIS 10", "PARIS 09", "PARIS 08", "PARIS 07", "PARIS 06", "PARIS 05", "PARIS 04", "PARIS 03", "PARIS 02", "PARIS 01")
-arr_marseille <- c("MARSEILLE 15", "MARSEILLE 14", "MARSEILLE 13", "MARSEILLE 12", "MARSEILLE 11", "MARSEILLE 10", "MARSEILLE 9", "MARSEILLE 8", "MARSEILLE 7", "MARSEILLE 6", "MARSEILLE 5", "MARSEILLE 4", "MARSEILLE 3", "MARSEILLE 2", "MARSEILLE 1")
-arr_lyon <- c("LYON 9", "LYON 8", "LYON 7", "LYON 6", "LYON 5", "LYON 4", "LYON 3", "LYON 2", "LYON 1")
+arr_marseille <- c("MARSEILLE 15", "MARSEILLE 14", "MARSEILLE 13", "MARSEILLE 12", "MARSEILLE 11", "MARSEILLE 10", "MARSEILLE 09", "MARSEILLE 08", "MARSEILLE 07", "MARSEILLE 06", "MARSEILLE 05", "MARSEILLE 04", "MARSEILLE 03", "MARSEILLE 02", "MARSEILLE 01")
+arr_lyon <- c("LYON 09", "LYON 08", "LYON 07", "LYON 06", "LYON 05", "LYON 04", "LYON 03", "LYON 02", "LYON 01")
 
 #stockage des valeurs de latitude et longitude correspondant aux villes de arr
 arr_lat_paris <- c("PARIS 20"=48.864527, "PARIS 19"=48.864527, "PARIS 18"=48.864527, "PARIS 17"=48.864527, "PARIS 16"=48.864527, "PARIS 15"=48.864527, "PARIS 14"=48.864527, "PARIS 13"=48.864527, "PARIS 12"=48.864527, "PARIS 11"=48.864527, "PARIS 10"=48.864527, "PARIS 09"=48.864527, "PARIS 08"=48.864527, "PARIS 07"=48.864527, "PARIS 06"=48.864527, "PARIS 05"=48.864527, "PARIS 04"=48.864527, "PARIS 03"=48.864527, "PARIS 02"=48.864527, "PARIS 01"=48.864527)
 arr_long_paris <- c("PARIS 20"=2.416170, "PARIS 19"=2.416170, "PARIS 18"=2.416170, "PARIS 17"=2.416170, "PARIS 16"=2.416170, "PARIS 15"=2.416170, "PARIS 14"=2.416170, "PARIS 13"=2.416170, "PARIS 12"=2.416170, "PARIS 11"=2.416170, "PARIS 10"=2.416170, "PARIS 09"=2.416170, "PARIS 08"=2.416170, "PARIS 07"=2.416170, "PARIS 06"=2.416170, "PARIS 05"=2.416170, "PARIS 04"=2.416170, "PARIS 03"=2.416170, "PARIS 02"=2.416170, "PARIS 01"=2.416170)
 
-arr_lat_lyon <- c("LYON 9"=45.772519, "LYON 8"=45.772519, "LYON 7"=45.772519, "LYON 6"=45.772519, "LYON 5"=45.772519, "LYON 4"=45.772519, "LYON 3"=45.772519, "LYON 2"=45.772519, "LYON 1"=45.772519)
-arr_long_lyon <- c("LYON 9"=4.858519, "LYON 8"=4.858519, "LYON 7"=4.858519, "LYON 6"=4.858519, "LYON 5"=4.858519, "LYON 4"=4.858519, "LYON 3"=4.858519, "LYON 2"=4.858519, "LYON 1"=4.858519)
+arr_lat_lyon <- c("LYON 09"=45.760339, "LYON 08"=45.760339, "LYON 07"=45.760339, "LYON 06"=45.760339, "LYON 05"=45.760339, "LYON 04"=45.760339, "LYON 03"=45.760339, "LYON 02"=45.760339, "LYON 01"=45.760339)
+arr_long_lyon <- c("LYON 09"=4.836629, "LYON 08"=4.836629, "LYON 07"=4.836629, "LYON 06"=4.836629, "LYON 05"=4.836629, "LYON 04"=4.836629, "LYON 03"=4.836629, "LYON 02"=4.836629, "LYON 01"=4.836629)
 
-arr_lat_marseille <- c("MARSEILLE 15"=43.292519, "MARSEILLE 14"=43.292519, "MARSEILLE 13"=43.292519, "MARSEILLE 12"=43.292519, "MARSEILLE 11"=43.292519, "MARSEILLE 10"=43.292519, "MARSEILLE 9"=43.292519, "MARSEILLE 8"=43.292519, "MARSEILLE 7"=43.292519, "MARSEILLE 6"=43.292519, "MARSEILLE 5"=43.292519, "MARSEILLE 4"=43.292519, "MARSEILLE 3"=43.292519, "MARSEILLE 2"=43.292519, "MARSEILLE 1"=43.292519)
-arr_long_marseille <- c("MARSEILLE 15"=5.372519, "MARSEILLE 14"=5.372519, "MARSEILLE 13"=5.372519, "MARSEILLE 12"=5.372519, "MARSEILLE 11"=5.372519, "MARSEILLE 10"=5.372519, "MARSEILLE 9"=5.372519, "MARSEILLE 8"=5.372519, "MARSEILLE 7"=5.372519, "MARSEILLE 6"=5.372519, "MARSEILLE 5"=5.372519, "MARSEILLE 4"=5.372519, "MARSEILLE 3"=5.372519, "MARSEILLE 2"=5.372519, "MARSEILLE 1"=5.372519)
+arr_long_marseille <- c("MARSEILLE 15"=5.372519, "MARSEILLE 14"=5.372519, "MARSEILLE 13"=5.372519, "MARSEILLE 12"=5.372519, "MARSEILLE 11"=5.372519, "MARSEILLE 10"=5.372519, "MARSEILLES 09"=5.372519, "MARSEILLE 08"=5.372519, "MARSEILLE 07"=5.372519, "MARSEILLE 06"=5.372519, "MARSEILLE 05"=5.372519, "MARSEILLE 04"=5.372519, "MARSEILLE 03"=5.372519, "MARSEILLE 02"=5.372519, "MARSEILLE 01"=5.372519)
+arr_lat_marseille <- c("MARSEILLE 15"=43.296482, "MARSEILLE 14"=43.296482, "MARSEILLE 13"=43.296482, "MARSEILLE 12"=43.296482, "MARSEILLE 11"=43.296482, "MARSEILLE 10"=43.296482, "MARSEILLE 09"=43.296482, "MARSEILLE 08"=43.296482, "MARSEILLE 07"=43.296482, "MARSEILLE 06"=43.296482, "MARSEILLE 05"=43.296482, "MARSEILLE 04"=43.296482, "MARSEILLE 03"=43.296482, "MARSEILLE 02"=43.296482, "MARSEILLE 01"=43.296482)
 
 #modification des valeurs de latitude et longitude de Paris
 for(i in 1:length(data$latitude)) {
