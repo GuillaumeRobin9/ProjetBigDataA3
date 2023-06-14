2c7fc78f9102ba08f89759df678354bfe6f3b620
Guillaume Robin <guirobin37@gmail.com>
2023-06-14 09:15:26 +0200

    Visualisation map + hist + jeu donne

---
+++ Histogram.R
@@ -0,0 +1,18 @@
+data <- read.csv("data/stat_acc_V3.csv", sep = ";")
+
+# Histograme quantité d’accidents en fonction des tranches d’âges
+age <- as.numeric(data$age) # age = char -> numeric
+hist(age, main = "Histogramme des accidents en fonction des tranches d’âges", xlab = "Tranches d’âges", ylab = "Nombre d’accidents", col = "blue", border = "red", breaks = 10)
+
+
+# Moyenne mensuelle des accidents
+#monthAvg <-avg(data$mois)
+data$date <- as.Date(data$date)
+data$month <- format(data$date, "%b") # get month
+monthly_avg <- aggregate(data$Num_Acc, by = list(data$month), FUN = length) # get avg
+colnames(monthly_avg) <- c("Month", "AccidentCount")
+monthly_avg <- monthly_avg[order(monthly_avg$Month), ] # sort by month
+barplot(monthly_avg$AccidentCount, names.arg = monthly_avg$Month, main = "Moyenne mensuelle des accidents",  xlab = "Mois", ylab = "Nombre d'accidents", col = "blue", border = "red", breaks = 10)
+
+
+#df2 <- df[order(data$date, decreasing=TRUE),]
